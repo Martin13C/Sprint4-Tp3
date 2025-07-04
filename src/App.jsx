93f24ba3db7178a73d2070e56ec82462484d9612
Martin Castro <martin1313jmc@gmail.com>
2025-07-04 01:00:44 -0300

    Error en la importacion home-Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { CartProvider } from './context/CartContext';
 import { ModalsProvider } from './context/ModalsContext';
 import { ThemeProvider } from './context/ThemeContext';
-import Home from './pages/home';
+import Home from './pages/Home';
 
 
 function App() {
